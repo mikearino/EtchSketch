716feef59da22755a896026196905bc8c6c21181
Mike Lofgren <mikeylofgren@gmail.com>
2023-03-27 21:21:06 -0700

    add in divs dynamically.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hi')
+//select display div
+const display = document.querySelector('.container')
+
+for (i=0; i < 32; i++) {
+ let square = document.createElement('div')
+  square.classList.add('content')
+  square.textContent = 'This is the text-content!';
+  square.style.cssText = "background-color: blue;"
+  display.appendChild(square)
+}

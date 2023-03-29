1ca3d1e2daa8f77bf4a8d674891b813a5e10a5e9
Mike Lofgren <mikeylofgren@gmail.com>
2023-03-29 15:40:54 -0700

    lay out function for random color gen.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //select display div
 const display = document.querySelector('.container')
 
+let rando = () => {
+  let r = Math.random()*256|0 
+  let g = Math.random()*256|0 
+  let b = Math.random()*256|0
+  return [r,g,b]
+};
+//use this in a function for hover.
+console.log(rando())
+
+
 for (i=1; i<257; i++) {
  let square = document.createElement('div')
   square.classList.add('content')

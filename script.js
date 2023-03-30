e38679f000a97a1d85c969097ee3f849ff35bcd2
Mike Lofgren <mikeylofgren@gmail.com>
2023-03-30 14:12:40 -0700

    add event listener to divs for hover.

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ let rando = () => {
   let b = Math.random()*256|0
   return [r,g,b]
 };
-//use this in a function for hover.
 console.log(rando())
+//use this in a function for hover.
+
 
 
 for (i=1; i<257; i++) {
  let square = document.createElement('div')
   square.classList.add('content')
   square.textContent = `Div ${[i]}`;
+  square.addEventListener('mouseover', () => {console.log('wow')})
   square.style.cssText = "background-color: blue;"
   display.appendChild(square)
 }

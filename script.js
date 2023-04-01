7f0c278940b8efce495ab4853ca1559c41b7099a
Mike Lofgren <mikeylofgren@gmail.com>
2023-03-31 22:39:29 -0700

    wrote functionality for color change.

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 //select display div
 const display = document.querySelector('.container')
 
-let rando = () => {
-  let r = Math.random()*256|0 
-  let g = Math.random()*256|0 
-  let b = Math.random()*256|0
-  return [r,g,b]
-};
-console.log(rando())
-//use this in a function for hover.
-
-
-
 for (i=1; i<257; i++) {
  let square = document.createElement('div')
   square.classList.add('content')
   square.textContent = `Div ${[i]}`;
-  square.addEventListener('mouseover', () => {console.log('wow')})
-  square.style.cssText = "background-color: blue;"
+  square.addEventListener('mouseover', ()=> {
+  let r = Math.random()*256|0 
+  let g = Math.random()*256|0 
+  let b = Math.random()*256|0  
+  square.style.cssText = `background-color: rgb(${r},${g},${b})`})
   display.appendChild(square)
 }
+
